6bfceba3a884c9ea5e73f4149e303877db7ebd67
g <gabe.patgorski@gmail.com>
2023-10-01 20:55:13 -0700

    asdf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
         ))}
       </ul> */}
       
-      <div className='sofaheader lilbubble'> Registration closed. Check out the pages for a deeper look into the game. </div>
+      <div className='sofaheader lilbubble'> Registration OPEN for September 2023! </div>
 
 
       <div className='sofaheader'> Welcome to Tribal Couch aka Sofa Survivor 2023. </div>
@@ -30,15 +30,15 @@ function Home() {
 
 
       <div className='lilbubble pl-6 ml-6'>
-      <div className='pb-5'>Who will win Survivor 44? (first episode debuts <span className='text-slate-600'>March 1</span>)</div>
+      <div className='pb-5'>Who will win Survivor 45? (first episode debuts <span className='text-slate-600'>September 27</span>)</div>
       <div className='font-semibold'>Instructions</div>
         <div className='pl-2 pb-2'>*Choose a name, a energy (for fun), and your 3 favorite contestants on the TV show. This is your Tribe.</div>
         <div className='pl-2 pb-2'>*Entry fee is $100. All entry fees will be awarded to the winning tribe. If multiple tribes pick the winner, the prize pool is divided proportionally. Payment must be made outside of this website somehow.</div>
-        <div className='pl-2 pb-2'>*The deadline to submit entries is when episode #2 of the show is aired, the evening of <span className='text-slate-600'>March 8</span>.</div>
+        <div className='pl-2 pb-2'>*The deadline to submit entries is when episode #2 of the show is aired, the evening of <span className='text-slate-600'>October 3</span>.</div>
         <div className='pl-2 pb-2'>*If you need to modify your entry before the deadline, just make a new tribe. I will manually delete 'unwanted' tribes based on which entries are paid for.</div>
       </div>
 
-      <div className='pl-3 w-full'><Link to='/tribes' className='bg-blue-500 p-4 w-full drop-shadow-lg hover:bg-blue-600' >View Tribes &rarr;&rarr;</Link>
+      <div className='pl-3 w-full'><Link to='/new' className='bg-blue-500 p-4 w-full drop-shadow-lg hover:bg-blue-600' >ENTER A Tribe &rarr;&rarr;</Link>
       </div>
     </div>
   );
